2b79379da2b07102f2c97afe643eb7260de6fad2
Erika Versoza <christine.bock98@gmail.com>
2021-09-11 11:46:34 -0400

    used jQuery on bootstrap buttons to call modals for reserve and login. Added to scss to change title background color and text color

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,13 @@ $(function() {
         }
     });
 });
+
+
+$('#reserveButton').on('click', function(){
+    $('#reserveModal').modal('show');
+});
+
+
+$('#loginButton').on('click', function(){
+    $('#loginModal').modal('show');
+});
